59f4e25c4d64768b9994a9e6f5fdb17d4f244eeb
Ioan Andrei Bârsan <AndreiBarsan@users.noreply.github.com>
2022-10-18 22:13:56 -0400

    Start restructuring things a little

---
+++ pit30m/pit30m/camera.py
@@ -0,0 +1,10 @@
+
+CAM_NAMES = [
+    "hdcam_02_starboard_front_roof_wide",
+    "hdcam_04_starboard_rear_roof_wide",
+    "hdcam_08_port_rear_roof_wide",
+    "hdcam_10_port_front_roof_wide",
+    "hdcam_12_middle_front_roof_narrow_left",
+    "hdcam_12_middle_front_roof_narrow_right",
+    "hdcam_12_middle_front_roof_wide",
+]
--- pit30m/pit30m/cli.py
+++ pit30m/pit30m/cli.py
@@ -1,16 +1,25 @@
 
+import shutil
 import fire
+import shlex
 from typing import Tuple
+from lzma import LZMAError
 
 import numpy as np
 import json
 import open3d as o3d
+import subprocess
 import fsspec
 from urllib.parse import urlparse
+import tempfile
 import ipdb
 import lzma
 from tqdm import tqdm
 import multiprocessing as mp
+from joblib import Parallel, delayed
+from PIL import Image
+
+from pit30m.camera import CAM_NAMES
 
 
 # Opposite of zip
@@ -19,6 +28,7 @@ def unzip(l):
 
 class Pit30MCLI:
     def __init__(self):
+        self._read_pool = Parallel(n_jobs=mp.cpu_count())
         pass
 
     def diagnose(self, dataset_base: str):
@@ -28,6 +38,57 @@ class Pit30MCLI:
     def diagnose_log(self, dataset_base: str, log_uri: str) -> None:
         self._diagnose_lidar(dataset_base, log_uri)
         self._diagnose_misc(dataset_base, log_uri)
+
+    def multicam_demo(self, dataset_base: str, log_uri: str, out_dir: str) -> None:
+        in_fs = fsspec.filesystem(urlparse(dataset_base).scheme)
+        out_fs = fsspec.filesystem(urlparse(out_dir).scheme)
+
+        # Clockwise camera names, with the front wide in the middle
+        cams_clockwise = [
+            "hdcam_08_port_rear_roof_wide",
+            "hdcam_10_port_front_roof_wide",
+            "hdcam_12_middle_front_roof_wide",
+            "hdcam_02_starboard_front_roof_wide",
+            "hdcam_04_starboard_rear_roof_wide",
+        ]
+
+        video_fpaths = []
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            for cam in cams_clockwise:
+                ld = dataset_base + log_uri + "/cameras/" + cam
+                chunks = sorted(in_fs.ls(ld))
+
+                sample_img_uris = [
+                    s_uri
+                    for s_uri in in_fs.ls(chunks[17]) + in_fs.ls(chunks[18])
+                    if s_uri.endswith("webp")
+                ]
+
+                def copy_img(img_uri, out_dir):
+                    with in_fs.open(img_uri, "rb") as f:
+                        with out_fs.open(out_dir + "/" + img_uri.split("/")[-1], "wb") as out_f:
+                            out_f.write(f.read())
+
+                out_img_dir = tmp_dir + "/frames/" + cam
+                if not out_fs.exists(out_img_dir):
+                    out_fs.mkdir(out_img_dir)
+                print("ETL for", cam)
+                res = self._read_pool(delayed(copy_img)(img_uri, out_img_dir) for img_uri in sample_img_uris)
+
+                framerate = 10
+                subprocess.run(
+                    shlex.split(f"ffmpeg -framerate {framerate} -i {out_img_dir}/%*.day.webp -crf 20 -pix_fmt yuv420p " \
+                        f"-filter:v 'scale=-1:800' {out_img_dir}.mp4")
+                )
+                video_fpaths.append(f"{out_img_dir}.mp4")
+
+            # Stack the resulting videos horizontally with ffmpeg
+            print(video_fpaths)
+            subprocess.run(
+                shlex.split(f"ffmpeg -i {' -i '.join(video_fpaths)} -filter_complex hstack=inputs={len(video_fpaths)} " \
+                    f"{out_dir}/{log_uri}-sample-multicam.mp4")
+            )
+
 
     def _diagnose_misc(self, dataset_base: str, log_uri: str) -> None:
         """Loads and prints misc data, hopefully raising errors if something is corrupted."""
@@ -91,12 +152,14 @@ class Pit30MCLI:
         ld = dataset_base + log_uri + "/lidars/"
         dbp = urlparse(dataset_base)
         fs = fsspec.filesystem(dbp.scheme)
+
+        # TODO(andrei): Diagnose and check shape!
+
         with mp.Pool(processes=mp.cpu_count() - 1) as pool:
             for lid_dir in fs.ls(ld):
-                # sample = lid_dir + "/0032/003203.npz.xz"
                 all_pcd = []
                 all_int = []
-                n_chunks = 99999
+                n_chunks = 20000
                 # Sample every 'sample_rate' sweeps.
                 sample_rate = 5
                 lidar_chunks = sorted(fs.ls(lid_dir))
@@ -107,7 +170,9 @@ class Pit30MCLI:
                         lidar_files += sorted(fs.ls(chunk)[::sample_rate])
 
                 print(f"Will load {len(lidar_files)} LiDAR pcds.")
-                all_pcd, all_int = unzip(pool.map( _load, lidar_files))
+                all_pcd, all_int = unzip(pool.map(_load_lidar, lidar_files))
+                print(all_pcd[0].shape)
+                print(all_pcd[0].dtype)
                 # for sub_sample in tqdm(
                 #     with lzma.open(sub_sample) as lzma_file:
                 #         lidar = np.load(lzma_file)
@@ -131,15 +196,17 @@ class Pit30MCLI:
                 o3d.visualization.draw_geometries([pcd])
 
 
-    def hello(self, name: str):
-        print(f"Hello {name}!")
-
-def _load(lidar_uri: str) -> Tuple[np.ndarray, np.ndarray]:
+def _load_lidar(lidar_uri: str) -> Tuple[np.ndarray, np.ndarray]:
+    """Loads a single LiDAR sweep from a URI, reading a LZMA-compressed file."""
     fs = fsspec.filesystem(urlparse(lidar_uri).scheme)
     with fs.open(lidar_uri) as f:
-        with lzma.open(f) as lzma_file:
-            lidar = np.load(lzma_file)
-            return np.array(lidar["points"]), np.array(lidar["intensity"])
+        try:
+            with lzma.open(f) as lzma_file:
+                lidar = np.load(lzma_file)
+                return np.array(lidar["points"]), np.array(lidar["intensity"])
+        except LZMAError:
+            print("LZMA error reading LiDAR uri: ", lidar_uri)
+            raise
 
 
 if __name__ == "__main__":
