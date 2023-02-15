36a4e7e944b4aa125c6f650c1d6352b2ba36f270
Ioan Andrei Bârsan <AndreiBarsan@users.noreply.github.com>
2023-02-14 23:48:04 -0500

    More tests

--- pit30m/data/submap.py
+++ pit30m/data/submap.py
@@ -1,20 +1,19 @@
 import pickle as pkl
 import uuid
-import ipdb
+from typing import Mapping, Optional, Tuple, Union
 from uuid import UUID
-from typing import Mapping, Tuple, Union, Optional
 
-import numpy as np
+import fsspec
+import ipdb
 import matplotlib.pyplot as plt
 import numpy as np
-import fsspec
-from pyproj import CRS, Transformer
 from joblib import Memory
-
+from pyproj import CRS, Transformer
 
 # See: https://epsg.io/32617
 UTM_ZONE_IN_PITTSBURGH_CODE = 32617
 WGS84_CODE = 4326
+DEFAULT_SUBMAP_INFO_PREFIX = "submap_utm.pkl"
 
 
 memory = Memory(location="/tmp/pit30m", verbose=0)
---
+++ tests/data/log_reader_integration_test.py
@@ -0,0 +1,21 @@
+from pytest import fixture
+
+from pit30m.camera import CamName
+from pit30m.data.log_reader import LogReader
+from pit30m.data.submap import DEFAULT_SUBMAP_INFO_PREFIX, Map
+
+
+@fixture(name="real_map")
+def _real_map() -> Map:
+    return Map.from_submap_utm_uri(f"s3://pit30m/{DEFAULT_SUBMAP_INFO_PREFIX}/")
+
+@fixture(name="real_log_reader")
+def _real_log_reader(real_map: Map) -> LogReader:
+    # Creates a real log reader for a cool log with lots of snow
+    return LogReader("s3://pit30m/7e9b5978-0a52-401c-dcd1-65c8d9930ad8/", map=real_map)
+
+
+def test_real_log_read_index(real_log_reader: LogReader):
+    index = real_log_reader.get_cam_geo_index(CamName.PORT_REAR_WIDE)
+    assert len(index) > 1000
+    assert "webp" in index["rel_path"][42]
