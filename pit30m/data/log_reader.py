663bfef8e5a5361ef4e03d1f74c0787416bad168
Ioan Andrei Bârsan <AndreiBarsan@users.noreply.github.com>
2023-05-24 00:49:52 -0400

    Tests work

--- pit30m/data/log_reader.py
+++ pit30m/data/log_reader.py
@@ -9,6 +9,7 @@ from uuid import UUID
 import fsspec
 import lz4
 import numpy as np
+import numpy.typing as npt
 import utm
 from joblib import Memory
 from numpy.lib import recfunctions as rfn
@@ -51,6 +52,53 @@ UTM_ZONE_NUMBER = 17
 UTM_ZONE_LETTER = "N"
 
 PARTITIONS_BASEPATH = "s3://pit30m/partitions/"
+
+# Original dtype for unified raw pose arrays. Regular users should be using specialized getters, such as those for
+# continuous or map-relative poses, not the raw poses. '?' entries are bool but this does not register for some arcane
+# reason.
+RAW_POSE_DTYPE = np.dtype(
+    [
+        ("transmission_time", "<f8"),
+        ("transmission_sequence_counter", "<i8"),
+        ("capture_time", "<f8"),
+        ("poses_and_differentials_valid", "?"),
+        (
+            "map_relative",
+            [
+                ("submap", "S16"),
+                ("x", "<f8"),
+                ("y", "<f8"),
+                ("z", "<f8"),
+                ("yaw", "<f8"),
+                ("pitch", "<f8"),
+                ("roll", "<f8"),
+                ("pose_covariance", "<f8", (6, 6)),
+                ("valid", "?"),
+            ],
+        ),
+        (
+            "continuous",
+            [
+                ("x", "<f8"),
+                ("y", "<f8"),
+                ("z", "<f8"),
+                ("yaw", "<f8"),
+                ("pitch", "<f8"),
+                ("roll", "<f8"),
+                ("pose_covariance", "<f8", (6, 6)),
+                ("vx", "<f8"),
+                ("vy", "<f8"),
+                ("vz", "<f8"),
+                ("roll_rate", "<f8"),
+                ("pitch_rate", "<f8"),
+                ("yaw_rate", "<f8"),
+                ("velocity_covariance", "<f8", (6, 6)),
+                ("acceleration", "<f8", (3, 1)),
+                ("valid", "?"),
+            ],
+        ),
+    ]
+)
 
 
 def gps_to_unix_timestamp(gps_seconds: float) -> float:
@@ -225,17 +273,21 @@ class LogReader:
             return data
 
     @cached_property
-    def raw_pose_data(self) -> np.ndarray:
-        """Returns the raw pose array, which needs manual association with other data types. 100Hz.
+    def raw_pose_data(self) -> npt.NDArray[RAW_POSE_DTYPE]:
+        """Returns the internal raw pose array, which needs manual association with other data types. 100Hz.
 
         In practice, users should use the camera/LiDAR iterators instead.
-
-        TODO(andrei): Document dtype (users now have to manually check dtype to learn).
         """
-        pose_fpath = os.path.join(self._log_root_uri, self._pose_fname)
-        with self.fs.open(pose_fpath, "rb") as in_compressed_f:
+        # XXX hack for development, do not land
+        # pose_fpath = os.path.join(self._log_root_uri, self._pose_fname)
+        root = self._log_root_uri.replace("s3://pit30m/", "/mnt/data/pit30m/pose-backup-2023-04-18/")
+        fs = fsspec.filesystem("file")
+        # fs = self.fs
+        pose_fpath = os.path.join(root, self._pose_fname)
+        with fs.open(pose_fpath, "rb") as in_compressed_f:
             with lz4.frame.open(in_compressed_f, "rb") as wgs84_f:
-                return np.load(wgs84_f)["data"]
+                raw_poses = np.load(wgs84_f)["data"]
+                return raw_poses
 
     @cached_property
     def continuous_pose_dense(self) -> np.ndarray:
@@ -284,11 +336,19 @@ class LogReader:
         # XXX(andrei): Document the timestamps carefully. Remember that GPS time, if applicable, can be confusing!
         for pose in self.raw_pose_data:
             # TODO(andrei): Custom, interpretable dtype!
+            # Handle submap IDs which were truncated upon encoded due to ending with a zero.
+            sid = pose["map_relative"]["submap"]
+            # actual_pad = len(sid) < 16
+            # if actual_pad:
+            #     print(len(sid))
+            padded_submap_id = sid.ljust(16, b"\x00")
+            # if actual_pad:
+            #     print(len(padded_submap_id))
             pose_data.append(
                 (
                     pose["capture_time"],
                     pose["poses_and_differentials_valid"],
-                    pose["map_relative"]["submap"],
+                    padded_submap_id,
                     pose["map_relative"]["x"],
                     pose["map_relative"]["y"],
                     pose["map_relative"]["z"],
@@ -328,8 +388,7 @@ class LogReader:
         """
         mrp = self.map_relative_poses_dense
         xyzs = rfn.structured_to_unstructured(mrp[["x", "y", "z"]])
-        # Handle submap IDs which were truncated upon encoded due to ending with a zero.
-        submaps = [UUID(bytes=submap_uuid_bytes.ljust(16, b"\x00")) for submap_uuid_bytes in mrp["submap_id"]]
+        submaps = [UUID(bytes=submap_uuid_bytes) for submap_uuid_bytes in mrp["submap_id"]]
 
         try:
             xys = self._map.to_utm(xyzs, submaps)
--- tests/submap_test.py
+++ tests/submap_test.py
@@ -2,8 +2,14 @@ import uuid
 from uuid import UUID
 
 import numpy as np
+import pytest
 
-from pit30m.data.submap import Map
+from pit30m.data.log_reader import LogReader
+from pit30m.data.submap import (
+    LOG_ID_TO_MISSING_SUBMAPS,
+    Map,
+    SubmapPoseNotFoundException,
+)
 
 
 def test_singleton():
@@ -26,9 +32,9 @@ def test_utm():
 
     fake_poses = np.array(
         [
-            [7, 7],
-            [1, 1],
-            [-2, 0.3],
+            [7, 7, 0],
+            [1, 1, 0],
+            [-2, 0.3, 16],
         ]
     )
 
@@ -37,9 +43,9 @@ def test_utm():
         utm_coords,
         np.array(
             [
-                [7, 7],
-                [11, 11],
-                [18, 20.3],
+                [7, 7,0],
+                [11, 11,0],
+                [18, 20.3,16],
             ]
         ),
     )
@@ -50,7 +56,7 @@ def test_wgs84():
 
     fake_poses = np.array(
         [
-            [0.33, -0.33],
+            [0.33, -0.33, 0.0],
         ]
     )
     fake_submap_ids = [uuid.uuid3(uuid.NAMESPACE_URL, "pratt")]
@@ -58,4 +64,34 @@ def test_wgs84():
 
     wgs84_coords = map.to_wgs84(fake_poses, fake_submap_ids)
     # GT computed using epsg.io
-    np.testing.assert_allclose(wgs84_coords, np.array([[43.531002, -79.3625012]]))
+    np.testing.assert_allclose(wgs84_coords, np.array([[43.531002, -79.3625012, 0.0]]))
+
+
+def test_missing_submap_utm():
+    """Integration test for handling the very few submaps with no UTM coordinates."""
+    map_ = Map()
+
+    for log_id in LOG_ID_TO_MISSING_SUBMAPS.keys():
+        lr = LogReader(log_root_uri=f"s3://pit30m/{log_id}/")
+        sids = []
+        # sids = [UUID(bytes=submap_uuid_bytes) for submap_uuid_bytes in lr.map_relative_poses_dense["submap_id"]]
+        for submap_uuid_bytes in lr.map_relative_poses_dense["submap_id"]:
+            submap_uuid_bytes = submap_uuid_bytes.ljust(16, b"\x00")
+            sids.append(UUID(bytes=submap_uuid_bytes))
+
+        mrp_xyz = np.hstack((lr.map_relative_poses_dense["x"][:, np.newaxis],
+                         lr.map_relative_poses_dense["y"][:, np.newaxis],
+                         lr.map_relative_poses_dense["z"][:, np.newaxis],
+                         ))
+        with pytest.raises(SubmapPoseNotFoundException):
+            utm_poses = map_.to_utm(mrp_xyz, sids)
+            assert np.isnan(utm_poses.ravel()).sum() == 0
+        with pytest.raises(SubmapPoseNotFoundException):
+            utm_poses = map_.to_utm(mrp_xyz, sids, strict=True)
+            assert np.isnan(utm_poses.ravel()).sum() == 0
+
+        utm_poses = map_.to_utm(mrp_xyz, sids, strict=False)
+        assert np.isnan(utm_poses.ravel()).sum() > 0
+        assert np.isnan(utm_poses.ravel()).sum() % 3 == 0   # A row is either all NaN or all non-NaN
+
+
